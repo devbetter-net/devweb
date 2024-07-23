4d7a4ab9ca895a1c023544ade4965bfab7f6db11
kyle <chitmeo@chitmeo.com>
2024-07-23 16:18:29 +0700

    feat: Add footer to the website
    This commit adds a footer to the website by creating a new file `_Footer.cshtml` in the `Views/Shared` folder. The footer includes copyright information and a link to the privacy policy page. This change enhances the user experience by providing consistent branding and navigation throughout the website.
    
    Note: The commit message has been generated based on the provided code changes and recent commits.

---
+++ src/Dev.Web/Components/HeaderViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dev.Web.Components;
+
+public class HeaderViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
